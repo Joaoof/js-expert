d094d099cea7fd1661626914740074ddd305c1b6
Joaoof <joaodeus400@gmail.com>
2023-03-03 11:08:40 -0300

    Implemente alternativa para usar a camera no Firefox com web workers

--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -1,12 +1,32 @@
 import Camera from "../../../lib/shared/camera.js"
+import { supportsWorkerType } from "../../../lib/shared/util.js"
 import Controller from "./controller.js"
 import Service from './service.js'
 import View from "./view.js"
 
+async function getWorker() {
+  if(supportsWorkerType()) {
+    console.log('Suporta')
+    const worker = new Worker('./src/worker.js', {type: 'module'})
+    return worker
+  }  
+  
+  const workerMock = {
+    async postMessage() {},
+    onmessage(msg) {}
+  }
+   console.log('Não suporta')
+   return workerMock
+
+} 
+
+const worker = await getWorker()
+console.log({worker})
+
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
-  async initalize() {
+  async initialize() {
     return Controller.initialize({
       view: new View({}),
       service: new Service({ 
