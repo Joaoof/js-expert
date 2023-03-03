d1d3aae382c3d2b50322cfe377e0050baa20dfdf
Joaoof <joaodeus400@gmail.com>
2023-03-03 13:56:34 -0300

    Testando os imports

--- classes/class01/pages/video-player/src/worker.js
+++ classes/class01/pages/video-player/src/worker.js
@@ -1,3 +1,12 @@
+import "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"
+import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
+import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
+import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
+import Service from "./service"
+// no processo principal é window
+// no worker é self
+debugger
+const {tf, faceLandmarksDetection} = self
 onmessage = ({ data }) => {
   console.log('worker', data)
   postMessage({
