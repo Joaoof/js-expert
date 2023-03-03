eab49014a7c19028a1139777810d56af89c5ad6f
Joaoof <joaodeus400@gmail.com>
2023-03-03 13:40:32 -0300

    Criando uma função de log para o status de reconhecimento de olhos

--- classes/class01/pages/video-player/src/controller.js
+++ classes/class01/pages/video-player/src/controller.js
@@ -4,14 +4,23 @@ export default class Controller {
   constructor({view, service}) {
     this.#view = view
     this.#service = service
+
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
   }
 
   static async initialize(deps) {
     const controller = new Controller(deps)
+    controller.log('not yet detecting eye blink! click in thw button to start')
     return controller.init()
   }
 
   async init() {
     console.log('init')
+  }
+  log(text) {
+    this.#view.log(`logger: ${text}`)
+  }  
+  onBtnStart() {
+    this.log('Initializing detection...')
   }
 }
